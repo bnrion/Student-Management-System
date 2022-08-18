1e9b3298806a44312a289e2bc98958de35ae2c04
bnrion <75271856+bnrion@users.noreply.github.com>
2022-08-19 00:03:48 +0700

    SMS Project
    Beans (IoC), Java Stream, Java Mail Sender, Clean Coding with Lombok, Spring Data JPA, Spring Web, Maven, PostgreSQL, Tomcat

---
+++ src/test/java/SMS/Student/Management/System/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package SMS.Student.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
